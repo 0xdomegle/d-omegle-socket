4f207635cb2e6f05535c0a55654f6b32f1f7f341
0xdev <zinzuvadiyameet98@gmail.com>
2024-03-08 16:49:23 +0530

    starting to build v2

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const http = require('http');
+var cors = require("cors");
+
+const { Server } = require("socket.io");
+
+const PORT = process.env.PORT || 1629;
+
+const app = express();
+app.use(cors());
+
+const expressServer = http.createServer({}, app);
+const io = new Server(expressServer, {
+    cors: {
+        origins: ["https://app.0xdomegle.com/", "http://localhost:5173/"],
+    },
+});
+
+let users = [];
+
+function matchUsers() {
+    console.log(users.length, "users are available");
+    if (users.length < 2) return;
+
+    const offererIndex = Math.floor(Math.random() * users.length);
+    let answererIndex = Math.floor(Math.random() * users.length);
+
+    while (offererIndex === answererIndex) {
+        answererIndex = Math.floor(Math.random() * users.length);
+    }
+
+    const offerer = users[offererIndex];
+    const answerer = users[answererIndex];
+
+    users.splice(offererIndex, 1);
+    users.splice(
+        answererIndex > offererIndex ? answererIndex - 1 : answererIndex,
+        1,
+    );
+    const session = new Session(offerer, answerer);
+
+    io.to(offerer.socketId).emit("createOffer", { offererID: offerer.socketId, answererID: answerer.socketId });
+}
+
+io.on("connection", (socket) => {
+
+    socket.emit("connected", {
+        localUserSocketID: socket.id,
+    })
+
+    socket.on("adminUser", (data) => {
+        const user = new User(data.socketId, data.address);
+        users.push(user);
+        matchUsers();
+    });
+
+    socket.on("sendOffer", (data) => {
+        socket.to(data.peers.answererID).emit("createAnswer", data)
+    });
+
+    socket.on("sendAnswer", (data) => {
+        socket.to(data.peers.offererID).emit("reciveAnswer", data)
+    });
+
+    socket.on("exchangeCandidates", (data) => {
+        console.log(data.remoteSocketId);
+        socket.to(data.remoteSocketId).emit("IceCandidateRecived", data);
+    });
+
+    socket.on("changeSession", (data) => { });
+
+    socket.on("disconnect", (data) => { });
+});
+
+expressServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+class User {
+    constructor(_socketID, _address) {
+        this.socketId = _socketID;
+        this.address = _address;
+    }
+}
+
+class Session {
+    constructor(_offerer, _senderer) {
+        this.sessionId = Date.now();
+        this.offerer = _offerer;
+        this.answerer = _senderer;
+    }
+}
--- socket.cjs
+++ socket.cjs
@@ -1,6 +1,6 @@
-const express = require('express');
-const http = require('http');
-var cors = require('cors')
+const express = require("express");
+const http = require("http");
+var cors = require("cors");
 
 const { User, Session, users, sessions, IceCandidates, iceCandidates } = require("./algo");
 const { Server } = require("socket.io");
@@ -10,80 +10,88 @@ app.use(cors());
 
 const server = http.createServer({}, app);
 const io = new Server(server, {
-    cors: {
-        origins: "https://app.0xdomegle.com/",
-    }
+  cors: {
+    origins: ["https://app.0xdomegle.com/", "http://localhost:5173/"],
+  },
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 1629;
 
 function matchUsers() {
-    console.log(users.length, "users are available")
-    if (users.length < 2) return;
+  console.log(users.length, "users are available");
+  if (users.length < 2) return;
 
-    const offererIndex = Math.floor(Math.random() * users.length);
-    let answererIndex = Math.floor(Math.random() * users.length);
+  const offererIndex = Math.floor(Math.random() * users.length);
+  let answererIndex = Math.floor(Math.random() * users.length);
 
-    while (offererIndex === answererIndex) {
-        answererIndex = Math.floor(Math.random() * users.length);
-    }
+  while (offererIndex === answererIndex) {
+    answererIndex = Math.floor(Math.random() * users.length);
+  }
 
-    const offerer = users[offererIndex];
-    const answerer = users[answererIndex];
+  const offerer = users[offererIndex];
+  const answerer = users[answererIndex];
 
-    // const offererCandidate = iceCandidates.find(candidate => offerer.address === candidate.address);
-    // const answererCandidate = iceCandidates.find(candidate => answerer.address === candidate.address);
+  users.splice(offererIndex, 1);
+  users.splice(
+    answererIndex > offererIndex ? answererIndex - 1 : answererIndex,
+    1,
+  );
+  const session = new Session(offerer, answerer);
+  sessions.push(session);
 
-    // console.log("offerCandidate", offererCandidate);
-    // console.log("answerCandidate", answererCandidate);
-
-    users.splice(offererIndex, 1);
-    users.splice(answererIndex > offererIndex ? answererIndex - 1 : answererIndex, 1);
-
-    const session = new Session(offerer, answerer);
-    sessions.push(session);
-
-    io.to(answerer.socketId).emit('matched', { sessionId: session.sessionId, partner: offerer, user: answerer });
-
+  io.to(answerer.socketId).emit("matched", {
+    sessionId: session.sessionId,
+    partner: offerer,
+    user: answerer,
+  });
 }
 
-io.on('connection', (socket) => {
-    console.log("domegle connection esablish on", socket.id);
+io.on("connection", (socket) => {
+  console.log("domegle connection esablish on", socket.id);
 
-    socket.emit('connection-success', {
-        status: "connection-success",
-        socketId: socket.id,
+  socket.emit("connection-success", {
+    status: "connection-success",
+    socketId: socket.id,
+  });
+
+  socket.on("disconnect", () => {
+    console.log("connection disconnected on", socket.id);
+    const userIndex = users.findIndex((user) => user.socketId === socket.id);
+    if (userIndex !== -1) users.splice(userIndex, 1);
+  });
+
+  socket.on("admitUser", (data) => {
+    const user = new User(socket.id, data.userAddress, data.offer);
+    users.push(user);
+    matchUsers();
+  });
+
+  socket.on("passCandidates", (data) => {
+    socket.to(data.peerAddress).emit("getIceCandidate", data.candidate);
+  });
+  socket.on("submitAnswer", (data) => {
+    let session = sessions.find(
+      (session) => session.sessionId == data.sessionId,
+    );
+    session.answerer.sdp = data.answer;
+    socket.to(session.offerer.socketId).emit("getAnswer", {
+      sessionId: session.sessionId,
+      answer: session.answerer,
+      offerer: session.offerer,
     });
+  });
 
-    socket.on("disconnect", () => {
-        console.log("connection disconnected on", socket.id);
-        const userIndex = users.findIndex(user => user.socketId === socket.id);
-        if (userIndex !== -1) users.splice(userIndex, 1);
-
-        // const iceCandidate = iceCandidates.findIndex(candidate => candidate.socketId === socket.id);
-        // if (iceCandidate !== -1) iceCandidates.splice(iceCandidate, 1);
-
-    });
-
-    socket.on('admitUser', data => {
-        const user = new User(socket.id, data.userAddress, data.offer);
-        users.push(user);
-        matchUsers();
-    });
-
-    socket.on("passCandidates", (data) => {
-        console.log(data);
-        socket.to(data.peerAddress).emit("getIceCandidate", data.candidate);
-    });
-
-    socket.on('submitAnswer', data => {
-        let session = sessions.find(session => session.sessionId == data.sessionId);
-        session.answerer.sdp = data.answer;
-        socket.to(session.offerer.socketId).emit('getAnswer', { "sessionId": session.sessionId, "answer": session.answerer });
-    });
-
+  socket.on("sessionEnd", (data) => {
+    console.log(data);
+    if (data) {
+      socket
+        .to(data.remoteUserSocketId)
+        .emit("endSession", { sessionId: data.sessionId });
+    }
+    return;
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
